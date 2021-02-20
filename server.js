bf71ab6c4d26ee8ddd237233f88df66c3a28b621
Piotr Charkot <piotr.charkot@gmail.com>
2021-02-20 18:26:09 +0100

    t

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const Datastore = require('nedb');
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
-const bcrypt = require('bcrypt');
 const passport = require('passport');
 const flash = require('express-flash');
 const session = require('express-session');
